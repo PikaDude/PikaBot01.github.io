98ebd72cc81876832a6fc49e22cdb4dc87d16cc8
PikaDude <skon7826@outlook.com>
2016-06-11 16:40:53 +1000

    finish 404 page

---
+++ 404/sauce/music.js
@@ -0,0 +1,28 @@
+// Music player
+window.onload = function() {
+    var player = document.getElementById('audio');
+    var info = document.getElementById('song')
+
+    var songs = [
+      ['Sad Violin (Airhorn Edition)', '404/sauce/Sad Violin (Airhorn Edition).mp3'],
+	  ['Rick Astley - Never Gonna Give You Up', '404/sauce/Rick Astley - Never Gonna Give You Up.mp3'],
+	  ['Undertale Soundtrack - Dogsong', '404/sauce/dogsong.mp3'],
+	  ['Sad Violin', '404/sauce/Sad Violin.mp3'],
+	  ['Spooky Scary Skeleton Dance Remix', '404/sauce/Spooky Scary Skeleton Dance Remix.mp3'],
+    ]
+
+    function nextSong() {
+      changeSong(Math.floor(Math.random() * songs.length));
+    };
+
+    function changeSong(val) {
+      var newSong = songs[val][1];
+      song.innerHTML = 'Now Playing: ' + songs[val][0];
+      player.setAttribute('src', songs[val][1]);
+      player.load();
+      player.play();
+      player.addEventListener('ended', nextSong);
+    };
+
+    nextSong();
+};
