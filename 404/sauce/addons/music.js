1cc890e09be4ace60c4a1e514a9fadcbbeace07a
PikaDude <skon7826@outlook.com>
2016-06-11 16:56:39 +1000

    FINALLY DONE! (i hope)

--- 404/sauce/addons/music.js
+++ 404/sauce/addons/music.js
@@ -5,8 +5,8 @@ window.onload = function() {
 
     var songs = [
       ['Sad Violin (Airhorn Edition)', '404/sauce/music/Sad Violin (Airhorn Edition).mp3'],
-	  ['Rick Astley - Never Gonna Give You Up', '.404/sauce/music/Rick Astley - Never Gonna Give You Up.mp3'],
-	  ['Undertale Soundtrack - Dogsong', '404/sauce/music/dogsong.mp3'],
+	  ['Rick Astley - Never Gonna Give You Up', '.404/sauce/music/Rickroll'],
+	  ['Undertale Soundtrack - Dogsong', '404/sauce/music/Dogsong.mp3'],
 	  ['Sad Violin', '404/sauce/music/Sad Violin.mp3'],
 	  ['Spooky Scary Skeleton Dance Remix', '404/sauce/music/Spooky Scary Skeleton Dance Remix.mp3'],
     ]
