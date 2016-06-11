634c007c5dedf1b17ca36565d9649d4cb7a2f7f2
PikaDude <skon7826@outlook.com>
2016-06-11 16:58:05 +1000

    removed the problematic .

--- 404/sauce/addons/music.js
+++ 404/sauce/addons/music.js
@@ -5,7 +5,7 @@ window.onload = function() {
 
     var songs = [
       ['Sad Violin (Airhorn Edition)', '404/sauce/music/Sad Violin (Airhorn Edition).mp3'],
-	  ['Rick Astley - Never Gonna Give You Up', '.404/sauce/music/Rickroll.mp3'],
+	  ['Rick Astley - Never Gonna Give You Up', '404/sauce/music/Rickroll.mp3'],
 	  ['Undertale Soundtrack - Dogsong', '404/sauce/music/Dogsong.mp3'],
 	  ['Sad Violin', '404/sauce/music/Sad Violin.mp3'],
 	  ['Spooky Scary Skeleton Dance Remix', '404/sauce/music/Spooky Scary Skeleton Dance Remix.mp3'],
