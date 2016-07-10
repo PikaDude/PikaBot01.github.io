9076ff92ac4b3ee4d33729a3e70b27a5312e2369
PikaDude <skon7826@outlook.com>
2016-07-10 12:58:07 +1000

    test

--- test.js
+++
@@ -1,7 +0,0 @@
-$("button").click(function(){
-    $.getJSON("http://128.199.142.235/test.json", function(result){
-        $.each(result, function(i, field){
-            $("div").append(field + " ");
-        });
-    });
-}); 
