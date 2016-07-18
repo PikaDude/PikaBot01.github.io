a38c3f1ad1407b168099f74f8a336e2d0cc2e589
PikaDude <skon7826@outlook.com>
2016-07-18 20:49:33 +1000

    New Look! Profiles! I'll work on it more later though

--- PikaBot01/source/scripts/search.js
+++ PikaBot01/source/scripts/search.js
@@ -1,5 +1,5 @@
 function swag() {
-    var thingy = prompt("Enter a users ID (or type >stats in chat)", "");
+    var thingy = prompt("Enter a users ID (or type >profile in chat)", "");
 	if (thingy != null) {
 	window.location="http://pikadude.github.io/PikaBot01/profile?id=" + thingy
 }
