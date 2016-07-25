e0d83517a7cff9d32c885fa0ad1a8efdbe8cba97
PikaDude <skon7826@outlook.com>
2016-07-25 20:42:27 +1000

    pretty much finished profiles

--- PikaBot01/source/scripts/get.js
+++ PikaBot01/source/scripts/get.js
@@ -17,7 +17,7 @@ if (id == null || id == "")
 try
 {
                 $.ajax({
-                    url: "http://128.199.142.235/Stuff/users/" + id + ".json",
+                    url: "http://pikabot01.xyz/Stuff/users/" + id + ".json",
                     dataType: "text",
                     success: function(data) {
                     var json = $.parseJSON(data);
@@ -35,11 +35,13 @@ catch (woah)
     document.getElementById('number').textContent = "Failed To Load: User Not Found"
 	return;
 }
-var coinsbal = "http://128.199.142.235/Stuff/coins/" + id + ".txt"
-var gamblecount = "http://128.199.142.235/Stuff/gamble/count/" + id + ".txt"
-var caught = "http://128.199.142.235/Stuff/pokemon/caught/" + id + ".txt"
-var pokeballs = "http://128.199.142.235/Stuff/pokemon/pokeballs/" + id + ".txt"
-var bio = "http://128.199.142.235/Stuff/bio/" + id + ".txt"
+var coinsbal = "http://pikabot01.xyz/Stuff/coins/" + id + ".txt"
+var background = "http://pikabot01.xyz/Stuff/backgrounds/" + id + ".txt"
+var gamblecount = "http://pikabot01.xyz/Stuff/gamble/count/" + id + ".txt"
+var caught = "http://pikabot01.xyz/Stuff/pokemon/caught/" + id + ".txt"
+var pokeballs = "http://pikabot01.xyz/Stuff/pokemon/pokeballs/" + id + ".txt"
+var bio = "http://pikabot01.xyz/Stuff/bio/" + id + ".txt"
+var pants = "http://pikabot01.xyz/Stuff/pants/awards/" + id + "/"
 try
 {
     var rawFile = new XMLHttpRequest();
@@ -146,6 +148,68 @@ try
 }catch(woah)
 {
 document.getElementById('bio').textContent = "User has not set a bio (>profile setbio I am cool)";
+}
+var backgroundurl = "";
+try
+	{
+	    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", background, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+				var backgroundurl = "http://pikabot01.xyz/Stuff/background-images/" + allText;
+                document.getElementById('dankmemes').style = "background:url(" + backgroundurl + ") no-repeat center center fixed; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover;";
+            }
+        }
+    }
+    rawFile.send(null);
+}catch(woah)
+{
+}
+//
+try
+	{
+	    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", bio, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+				document.getElementById('pants1').textContent = "Thinking (and shitting) inside the box - Congratulations, all that potty training finally paid off.";
+            }
+        }
+    }
+    rawFile.send(null);
+}catch(woah)
+{
+document.getElementById('pants1').textContent = "Thinking (and shitting) inside the box - ???";
+}
+try
+	{
+	    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", bio, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+				document.getElementById('pants1').textContent = "Thinking (and shitting) inside the box - Congratulations, all that potty training finally paid off.";
+            }
+        }
+    }
+    rawFile.send(null);
+}catch(woah)
+{
+document.getElementById('pants1').textContent = "Thinking (and shitting) inside the box - ???";
 }
 document.getElementById('tableprogress').style = "width: 80%";
 document.getElementById('number').textContent = "80%";
--- PikaBot01/source/scripts/get2.js
+++ PikaBot01/source/scripts/get2.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-function getParameterByName(name, url) {
+	function getParameterByName(name, url) {
     if (!url) url = window.location.href;
     name = name.replace(/[\[\]]/g, "\\$&");
     var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
@@ -11,12 +11,43 @@ function getParameterByName(name, url) {
 var id = getParameterByName('id');
 if (id == null || id == "")
 {
-	window.alert("Invalid User");
+	document.getElementById('number').textContent = "Invalid User"
 	return;
 }
-var url = "http://128.199.142.235/Stuff/";
-function usuck(file) {
-	var rawFile = new XMLHttpRequest();
+try
+{
+                $.ajax({
+                    url: "http://pikabot01.xyz/Stuff/users/" + id + ".json",
+                    dataType: "text",
+                    success: function(data) {
+                    var json = $.parseJSON(data);
+					document.getElementById('name').textContent = json.name;
+					document.getElementById('description').Content = json.name + "'s Profile";
+					document.getElementById('title').textContent = "PikaBot01 - " + json.name + "'s Profile";
+					document.getElementById('avatar').src = json.avatar;
+					document.getElementById('discrim').textContent = "#" + json.discrim;
+					}
+				}
+				);
+}
+catch (woah)
+{
+    document.getElementById('number').textContent = "User Not Found"
+	return;
+}
+document.getElementById('tableprogress').style = "width: 6%";
+document.getElementById('number').textContent = "6%";
+var coinsbal = "http://pikabot01.xyz/Stuff/coins/" + id + ".txt"
+var background = "http://pikabot01.xyz/Stuff/backgrounds/" + id + ".txt"
+var gamblecount = "http://pikabot01.xyz/Stuff/gamble/count/" + id + ".txt"
+var caught = "http://pikabot01.xyz/Stuff/pokemon/caught/" + id + ".txt"
+var pokeballs = "http://pikabot01.xyz/Stuff/pokemon/pokeballs/" + id + ".txt"
+var bio = "http://pikabot01.xyz/Stuff/bio/" + id + ".txt"
+var pants = "http://pikabot01.xyz/Stuff/pants/awards/" + id + "/"
+// end start stuff, now to GET everything, amirite
+function get(file)
+{
+    var rawFile = new XMLHttpRequest();
     rawFile.open("GET", file, false);
     rawFile.onreadystatechange = function ()
     {
@@ -28,7 +59,86 @@ function usuck(file) {
             }
         }
     }
-	return allText;
+    rawFile.send(null);
+	return rawFile.responseText;
 }
-window.alert(usuck(id + "/coins/" + id + ".txt").allText);
+function FileExists(urlToFile)
+{
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", urlToFile, false);
+    xhr.send();
+     
+    if (xhr.status == "404") {
+        return false;
+    } else {
+        return true;
+    }
+}
+document.getElementById('caught').textContent = get(caught);
+document.getElementById('tableprogress').style = "width: 12%";
+document.getElementById('number').textContent = "12%";
+document.getElementById('pokeballs').textContent = get(pokeballs);
+document.getElementById('tableprogress').style = "width: 18%";
+document.getElementById('number').textContent = "18%";
+document.getElementById('coinsbal').textContent = get(coinsbal);
+document.getElementById('tableprogress').style = "width: 24%";
+document.getElementById('number').textContent = "24%";
+document.getElementById('gamblecount').textContent = get(gamblecount);
+document.getElementById('tableprogress').style = "width: 30%";
+document.getElementById('number').textContent = "30%";
+document.getElementById('bio').textContent = get(bio);
+document.getElementById('tableprogress').style = "width: 36%";
+document.getElementById('number').textContent = "36%";
+var backgroundurl = "http://pikabot01.xyz/Stuff/background-images/" + get(background);
+document.getElementById('dankmemes').style = "background:url(" + backgroundurl + ") no-repeat center center fixed; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover;";
+document.getElementById('tableprogress').style = "width: 42%";
+document.getElementById('number').textContent = "42%";
+//
+try {if (FileExists(pants + "1.txt"))
+document.getElementById('pants1').textContent = "Congratulations, all that potty training finally paid off."; }catch (woah){
+document.getElementById('pants1').textContent = "???";}
+document.getElementById('tableprogress').style = "width: 48%";
+document.getElementById('number').textContent = "48%";
+try {if (FileExists(pants + "2.txt"))
+document.getElementById('pants2').textContent = "It's not his fault the door was so hard to open.";}catch (woah){
+document.getElementById('pants2').textContent = "???";}
+document.getElementById('tableprogress').style = "width: 54%";
+document.getElementById('number').textContent = "54%";
+try {if (FileExists(pants + "3.txt"))
+document.getElementById('pants3').textContent = "Sometimes even zero effort is awarded.";}catch (woah){
+document.getElementById('pants3').textContent = "???";}
+document.getElementById('tableprogress').style = "width: 60%";
+document.getElementById('number').textContent = "60%";
+try {if (FileExists(pants + "4.txt"))
+document.getElementById('pants4').textContent = "Pants. They get you every time...";}catch (woah){
+document.getElementById('pants4').textContent = "???";}
+document.getElementById('tableprogress').style = "width: 66%";
+document.getElementById('number').textContent = "66%";
+try {if (FileExists(pants + "5.txt"))
+document.getElementById('pants5').textContent = "Giving up is never the answer. Or is it?";}catch (woah){
+document.getElementById('pants5').textContent = "???";}
+document.getElementById('tableprogress').style = "width: 72%";
+document.getElementById('number').textContent = "72%";
+try {if (FileExists(pants + "6.txt"))
+document.getElementById('pants6').textContent = "How convenient that you had those pills.";}catch (woah){
+document.getElementById('pants6').textContent = "???";}
+document.getElementById('tableprogress').style = "width: 78%";
+document.getElementById('number').textContent = "78%";
+try {if (FileExists(pants + "7.txt"))
+document.getElementById('pants7').textContent = "...to not poop for an extra five seconds.";}catch (woah){
+document.getElementById('pants7').textContent = "???";}
+try {if (FileExists(pants + "8.txt"))
+document.getElementById('pants8').textContent = "You shit before the game began!";}catch (woah){
+document.getElementById('pants8').textContent = "???";}
+document.getElementById('tableprogress').style = "width: 84%";
+document.getElementById('number').textContent = "84%";
+try {if (FileExists(pants + "1.txt") && FileExists(pants + "2.txt") && FileExists(pants + "3.txt") && FileExists(pants + "4.txt") && FileExists(pants + "5.txt") && FileExists(pants + "6.txt") && FileExists(pants + "7.txt") && FileExists(pants + "8.txt"))
+document.getElementById('pants9').textContent = "And you have a crown to prove it!";}catch (woah){
+document.getElementById('pants9').textContent = "???";}
+document.getElementById('tableprogress').style = "width: 100%";
+document.getElementById('number').textContent = "100%";
+document.getElementById('tableprogress').style = "display: none;";
+document.getElementById('tableload').style = "display: none;";
+document.getElementById('imBLIND').style = "display: none;";
+Materialize.showStaggeredList('#fancy')
 }
