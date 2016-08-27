2ca6db3084c7dbd4d1cebe9f3f0868a00280797b
PikaDude <skon7826@outlook.com>
2016-08-27 16:40:21 +1000

    snails

---
+++ projects/Snail Speed/assets/get.js
@@ -0,0 +1,26 @@
+window.onload = function() {
+	function snail(){
+                $.ajax({
+                    url: "http://pikabot01.xyz/snail/snail.json",
+                    dataType: "text",
+                    success: function(data) {
+                    var json = $.parseJSON(data);
+					document.getElementById('total').textContent = json.distance + " millimetres or " + parseInt(json.distance) / 10 + " centimetres or " + parseInt(json.distance) / 10 / 100 + " metres";
+					document.getElementById('uptime').textContent = json.uptime;
+					document.getElementById('status').textContent = json.status;
+					document.getElementById('speed').textContent = json.speed;
+					document.getElementById('mood').textContent = json.mood;
+					document.getElementById('time').textContent = json.time;
+					document.getElementById('weather').textContent = json.weather;
+					var weather;
+					if (json.weather === "rain") weather = "rain.gif";
+					if (json.weather === "clear") weather = "clear.jpg";
+					var url = json.tiem + "_" + weather;
+					document.getElementById('snail').style = "background:url(assets/" + url + ") no-repeat center center fixed; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover;";
+					window.setTimeout(snail, 3000);
+					}
+				}
+				);
+	}
+	snail()
+}
