6fc9c6e63d87e62a5dec33dc9503452784d8dfa0
PikaDude <skon7826@outlook.com>
2016-08-28 13:55:38 +1000

    wip

--- projects/Snail Speed/assets/scripts/noot.js
+++
@@ -1,9 +0,0 @@
-Push.create("Hello world!", {
-    body: "How's it hangin'?",
-    icon: 'assets/images/snail.png',
-    timeout: 4000,
-    onClick: function () {
-        window.focus();
-        this.close();
-    }
-});
